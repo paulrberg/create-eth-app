9ad23a4baf4d21449e3a0486df43c451ca7b26b7
FuzzysTodd <fuzzystodd@gmail.com>
2024-10-06 22:26:22 -0400

    master

---
+++ CoinMetadata.sol
@@ -0,0 +1,36 @@
+	
+Successful operation
+
+Media type
+
+application/json
+Controls Accept header.
+Example Value
+{
+  "success": true,
+  "data": [
+    {
+      "owner_address": "0xd6881b0c670c4594b622eb3974a9ea657b6f2de4f01f587cbe4dc19e224b5486",
+      "coin_type_hash": "91ceb1308a98389691e05158b07ed5f079ab78461a6bb8d5a4054b1bb5cb8bb6",
+      "coin_type": "0x1::aptos_coin::AptosCoin",
+      "amount": "181572800",
+      "last_transaction_version": 33339955,
+      "last_transaction_timestamp": "2022-11-17T16:10:18.000Z",
+      "coin_name": "Aptos Coin",
+      "coin_symbol": "APT",
+      "coin_decimals": 8
+    },
+    {
+      "owner_address": "0xd6881b0c670c4594b622eb3974a9ea657b6f2de4f01f587cbe4dc19e224b5486",
+      "coin_type_hash": "f3f82889c5c74d207f9b944cadca25273dd02e090f5bee17e7314c2a5854f345",
+      "coin_type": "0x881ac202b1f1e6ad4efcff7a1d0579411533f2502417a19211cfc49751ddb5f4::coin::MOJO",
+      "amount": "16264800000000",
+      "last_transaction_version": 9431649,
+      "last_transaction_timestamp": "2022-10-23T14:00:26.000Z",
+      "coin_name": "Mojito",
+      "coin_symbol": "MOJO",
+      "coin_decimals": 8
+    }
+  ],
+  "metadata": {}
+}
